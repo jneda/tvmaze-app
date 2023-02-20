7d7a764e40a66ed6afcfd9d6ac845f6b9157b468
jdane <julien.dane@gmail.com>
2023-02-20 16:05:04 +0100

    feat: use dompurify and html-rect-parser to display show summary info

--- src/components/ShowInfo.jsx
+++ src/components/ShowInfo.jsx
@@ -1,11 +1,17 @@
+import DOMPurify from "dompurify";
+import parse from "html-react-parser";
+
 export default function ShowInfo(props) {
   const { score, show } = props.data;
   const { name, summary } = show;
 
+  const sanitizedSummary = DOMPurify.sanitize(summary);
+  const summaryElement = parse(sanitizedSummary);
+
   return (
     <article>
       <h2>{name}</h2>
-      {summary}
+      {summaryElement}
     </article>
   );
 }
