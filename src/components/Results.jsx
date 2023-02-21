0c6bec250b50a97e8a5ae3031c5c7ee1c69e24de
jdane <julien.dane@gmail.com>
2023-02-21 10:17:26 +0100

    fix: chair screen interface bug

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -2,14 +2,9 @@ import ShowInfo from "./ShowInfo";
 
 export default function Results(props) {
   const { length, shows } = props.results;
-<<<<<<< HEAD
   const results = shows.map((show) => (
     // <pre key={result.id}>{JSON.stringify(result)}</pre>
     <ShowInfo key={show.show.id} data={show} />
-=======
-  const results = shows.map((data) => (
-    <ShowInfo key={data.show.id} data={data} />
->>>>>>> 2ee13b968e6e791a9a6b3a1d32b7cf274428e059
   ));
 
   return (
