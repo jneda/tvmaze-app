e4d388bd9a97e90726a20a6d9d188d333a1238c3
jdane <julien.dane@gmail.com>
2023-02-22 15:00:36 +0100

    style tweaks (image sizes mainly)

--- src/components/ShowInfo.jsx
+++ src/components/ShowInfo.jsx
@@ -4,7 +4,7 @@ import parse from "html-react-parser";
 export default function ShowInfo(props) {
   const { score, show } = props.data;
   const { name, summary, image } = show;
-  const imageUrl = image ? image.original : "";
+  const imageUrl = image ? image.medium : "";
 
   const sanitizedSummary = DOMPurify.sanitize(summary);
   const summaryElement = parse(sanitizedSummary);
